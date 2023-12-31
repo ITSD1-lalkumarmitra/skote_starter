8fa02d789f471fb7f75ff5ae0251e3b12c2ff52c
Shubham <idsd3-shubham>
2023-12-31 11:25:55 +0530

    avoid this message

--- src/pages/UserManagement/Student/index.js
+++ src/pages/UserManagement/Student/index.js
@@ -1,17 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { Row, Col, Card, CardHeader } from 'reactstrap';
 //import commponent/common file 
 import Breadcrumbs from "components/Common/Breadcrumb";
 
 const index = () => {
   return (
     <React.Fragment>
-        <div className="page-content">
+      <div className="page-content">
         <Breadcrumbs title="User" breadcrumbItem="Student's" />
         <div className="container-fluid">
-            hello
+          <Row>
+            <Col lg="12">
+              <Card>
+                <CardHeader>
+                  <div className="d-flex align-items-center">
+                    <h5 className="mb-0 card-title flex-grow-1">Student Lists</h5>
+                    <div className="flex-shrink-0">
+                      <Link to="#" className="btn btn-success me-1"> Add Student</Link>
+                    </div>
+                  </div>
+                </CardHeader>
+
+              </Card>
+            </Col>
+          </Row>
         </div>
-        </div>
+      </div>
     </React.Fragment>
   )
 }
